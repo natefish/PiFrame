21a3b2cab571943396e6731eb40b17e6b1e06195
natefish <nathan.reinhard@gmail.com>
2019-02-22 18:25:36 -0500

    Preparation for first release :-)

--- www/js/PiFrameMain.js
+++ www/js/PiFrameMain.js
@@ -1,49 +1,86 @@
-var xhttp = new XMLHttpRequest();
+var slideDelay = 30000;
+var imgPath = "images/sample/";
+var Randomize
 
-xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        stageSlides(this.responseXML);
+function getFile(filePath){
+    console.info("Loading file: " + filePath);
+
+    return new Promise(function(resolve, reject){
+        var xhttp = new XMLHttpRequest();
+
+        xhttp.open("GET", filePath, true);
+        xhttp.onload = function () {
+            resolve(this);
+        };
+        xhttp.onerror = function () {
+            reject(new Error("Unabled to load " + filePath));
+        };
+        xhttp.send();
+    });
+}
+
+async function initializeSettings(){
+    console.info("Initializing settings");
+
+    var res = await getFile("settings.txt");
+
+    if(res.status == 200){
+        var jsonDoc = JSON.parse(res.responseText);
+
+        if(jsonDoc.slideDelay != null){
+            console.info("Setting slideDelay (" + slideDelay + ") to " + jsonDoc.slideDelay);
+            slideDelay = jsonDoc.slideDelay;
+        }
     }
-};
-xhttp.open("GET", "images.xml", true);
-xhttp.send();
+}
+
+async function startSlideshow(){
+    var res = await getFile("images.xml");
+    if(res.status == 200){
+        stageSlides(res.responseXML);
+    }else{
+        stageSlides(getFile("images-example.xml").responseXML);
+    }
+}
 
 function preloadNextImage(imgIndex, imgNodes) {
     imgIndex++;
     if (imgIndex >= imgNodes.length) { imgIndex = 0 }
 
-    imgURL = imgNodes[imgIndex].childNodes[0].nodeValue;
+    imgURL = imgPath + imgNodes[imgIndex].childNodes[0].nodeValue;
 
     //Pre-load the image
     var nextImg = new Image();
     nextImg.src = imgURL;
 }
 
-function stageSlides(xmlDoc) {
-    var imgNodes = xmlDoc.getElementsByTagName("img");
-    var imgIndex = imgNodes.length;
+function loadImage(imgIndex,imgNodes){
     var imgURL = "";
 
-    if (imgIndex >= imgNodes.length) { imgIndex = 0 }
-
-    imgURL = imgNodes[imgIndex].childNodes[0].nodeValue;
+    imgURL = imgPath + imgNodes[imgIndex].childNodes[0].nodeValue;
 
     document.getElementById("photoDisplay").style.backgroundImage = "url('" + imgURL + "')";
     document.getElementById("bgFill").style.backgroundImage = "url('" + imgURL + "')";
 
     preloadNextImage(imgIndex, imgNodes);
 
+}
+
+function stageSlides(xmlDoc) {
+    var imgNodes = xmlDoc.getElementsByTagName("img");
+    var imgIndex = 0;
+
+    //TODO: Randomize picture selection by default, maybe use session storage
+
+    loadImage(imgIndex, imgNodes);
+
     setInterval(function () {
         imgIndex++;
         if (imgIndex >= imgNodes.length) { imgIndex = 0 }
 
-        imgURL = imgNodes[imgIndex].childNodes[0].nodeValue;
+        loadImage(imgIndex, imgNodes);
 
-        document.getElementById("photoDisplay").style.backgroundImage = "url('" + imgURL + "')";
-        document.getElementById("bgFill").style.backgroundImage = "url('" + imgURL + "')";
-
-        preloadNextImage(imgIndex, imgNodes);
-    }, 30000);
+    }, slideDelay);
 }
 
 function openNav() {
